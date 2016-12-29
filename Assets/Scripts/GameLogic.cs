06cc9c9ac7742902d0b22c8f575fb6973b2ad6c4
Kristin Dragos <KristinDragos@gmail.com>
2016-12-28 23:33:08 -0800

    Added start and ending UIs. Added movement through puzzle.
    FEATURE: Added starting UI with start and quit buttons.
    FEATURE: Added ending UI with restart and quit buttons.
    FEATURE: Used iTween to move player through puzzle. (From start to play to
    finish points.)
    FEATURE: Hooked up buttons on both UI panels.
    FEATURE: Added some torches on the outside of the dungeon to add some
    environment to the outside.

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -3,22 +3,77 @@ using System.Collections;
 
 public class GameLogic : MonoBehaviour {
 
-	public GameObject player;
-	public GameObject startUI, restartUI;
-
+	public GameObject player; 
+	public GameObject startUI, restartUI; 
+	public GameObject startPoint, playPoint, restartPoint;
+	public GameObject gameBoard;
+		
+	public bool playerWon = false;
 
 	// Use this for initialization
 	void Start () {
-
+		player.transform.position = startPoint.transform.position;
 	}
 
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetMouseButtonDown (0) && player.transform.position==playPoint.transform.position) {
+			puzzleSuccess ();
+		}
+	}
 
+	public void startPuzzle() { //Begin the puzzle sequence
+		toggleUI(2);
+		iTween.MoveTo (player, 
+			iTween.Hash (
+				"position", playPoint.transform.position, 
+				"time", 3, 
+				"easetype", "linear"
+			)
+		);
+	}
+
+	public void resetPuzzle() { //Reset the puzzle sequence
+		player.transform.position = startPoint.transform.position;
+		toggleUI (1);
+	}
+
+
+	public void puzzleSuccess() { //Do this when the player gets it right
+		toggleUI(3);
+		iTween.MoveTo (player, 
+			iTween.Hash (
+				"position", restartPoint.transform.position, 
+				"time", 3, 
+				"easetype", "linear"
+			)
+		);
+	}
+
+	public void quitPuzzle() {
+		Application.Quit ();
+		Debug.Log ("Quitting the game");
 	}
 
 	public void toggleUI() {
 		startUI.SetActive (!startUI.activeSelf);
 		restartUI.SetActive (!restartUI.activeSelf);
+	}
+
+	public void toggleUI(int spot){
+		if (spot == 1) {
+			startUI.SetActive (true);
+			gameBoard.SetActive (false);
+			restartUI.SetActive (false);
+		} else if (spot == 2) {
+			startUI.SetActive (false);
+			gameBoard.SetActive (true);
+			restartUI.SetActive (false);
+		} else {
+			startUI.SetActive (false);
+			gameBoard.SetActive (false);
+			restartUI.SetActive (true);
+		}
+
 	}
 }
